ad7731de1c25cd833e798a342af8534d7678a1cf
Chris Nuernberger <chris@thinktopic.com>
2018-12-18 09:35:15 -0700

    Created script to auto-build docs.

---
+++ scripts/build-docs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DEPSVIZ_DEPS='{:deps {cnuernber/depsviz {:mvn/version "0.3"}}}'
+
+
+clojure -Sdeps "$DEPSVIZ_DEPS" -m cnuernber.depsviz -i test/data/deps.edn --no-view --output-file docs/full-example.pdf
+clojure -Sdeps "$DEPSVIZ_DEPS" -m cnuernber.depsviz -i test/data/deps.edn --prune --no-view --output-file docs/prune-example.pdf
+clojure -Sdeps "$DEPSVIZ_DEPS" -m cnuernber.depsviz -i test/data/deps.edn --focus maven-resolver-api --no-view --output-file docs/focus-example.pdf
+
