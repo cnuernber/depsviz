e64c59721998e3467d4c7a86ec045707f090284c
Chris Nuernberger <chris@thinktopic.com>
2018-12-19 15:13:23 -0700

    0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cnuernber/depsviz "0.4-SNAPSHOT"
+(defproject cnuernber/depsviz "0.4"
   :description "View deps.edn transitive dependencies.  Inspred "
   :url "http://github.com/cnuernber/depsviz"
   :license {:name "Eclipse Public License"
