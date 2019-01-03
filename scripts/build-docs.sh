27bad477ae519f631ee80ecc6e18db0a83789262
Chris Nuernberger <chris@thinktopic.com>
2019-01-03 10:54:01 -0700

    upgrade.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEPSVIZ_DEPS='{:deps {cnuernber/depsviz {:mvn/version "0.8"}}}'
+DEPSVIZ_DEPS='{:deps {cnuernber/depsviz {:mvn/version "0.9"}}}'
 
 
 clojure -Sdeps "$DEPSVIZ_DEPS" -m cnuernber.depsviz -i test/data/deps.edn --no-view --output-file docs/full-example.pdf
