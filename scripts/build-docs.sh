cfaeb8903a6785d5e01de1cd2cf593c84b440b8f
Chris Nuernberger <chris@thinktopic.com>
2019-01-03 10:24:47 -0700

    upgrade to latest with latest deps.alpha.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEPSVIZ_DEPS='{:deps {cnuernber/depsviz {:mvn/version "0.7"}}}'
+DEPSVIZ_DEPS='{:deps {cnuernber/depsviz {:mvn/version "0.8"}}}'
 
 
 clojure -Sdeps "$DEPSVIZ_DEPS" -m cnuernber.depsviz -i test/data/deps.edn --no-view --output-file docs/full-example.pdf
